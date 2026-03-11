c1c20d911e599c5725f82184af985c21898b2f24
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-03-11 17:45:31 +0530

    Added Project model to db

--- web/qdeploy/models.py
+++ web/qdeploy/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from typing import TYPE_CHECKING
+
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    repo = models.URLField()
+    deployed = models.BooleanField()
--- web/qdeploy/views.py
+++ web/qdeploy/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Project
+
 # Create your views here.
+
 
 class HomepageView(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'dashboard/index.html')
+        projects = Project.objects.all()
+        return render(request, "dashboard/index.html", context={"projects": projects})
